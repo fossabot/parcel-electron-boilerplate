f72f181b24c7f17670f472d0857a04bcc554c6d4
Elmer Yu <ak64th@gmail.com>
2019-07-12 14:38:15 +0800

    make hmr work

--- renderer/index.js
+++ renderer/index.js
@@ -1,3 +1,8 @@
+import { remote } from 'electron';
+
+console.log('Electron:', process.versions.electron);
+console.log(remote.app);
+
 window.onload = function(){
-  document.getElementById('app').innerHTML = 'Hello, world.';
+  document.getElementById('app').innerHTML = 'Hello, world';
 }
