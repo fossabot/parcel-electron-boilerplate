6b0a7a68fcfafdf055822b9f25a787b8249dfaa4
Elmer Yu <ak64th@gmail.com>
2019-07-12 19:21:02 +0800

    chore: add browserslist config

--- renderer/index.js
+++ renderer/index.js
@@ -3,6 +3,8 @@ import { remote } from 'electron';
 console.log('Electron:', process.versions.electron);
 console.log(remote.app);
 
+const name = 'world';
+
 window.onload = function(){
-  document.getElementById('app').innerHTML = 'Hello, world';
+  document.getElementById('app').innerHTML = `Helle, ${name}.`;
 }
